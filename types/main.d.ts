6b149c6aa9f828b6b80175a6a16108692e3c3da5
Mattia <mattia@wonderflow.ai>
2021-07-06 18:04:58 +0200

    chore: add types

---
+++ types/main.d.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/**
+ * Default CSS definition for typescript
+ */
+ declare module '*.css' {
+  const content: { [className: string]: string }
+  export default content
+}
+
+declare module 'csstype' {
+  interface Properties {
+    [index: string]: any;
+  }
+}
+
+declare module '*.svg' {
+  const svgUrl: string
+  const svgComponent: React.StatelessComponent<React.SVGAttributes<SVGElement>>
+  export default svgUrl
+  export { svgComponent as ReactComponent }
+}
