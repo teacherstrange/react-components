8349adf25b5975babfd89120983affa383c6b310
Mattia <mattia@wonderflow.ai>
2021-07-06 17:45:43 +0200

    feat: add postcss and css modules support

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,15 @@ module.exports = {
   addons: [
     "@storybook/addon-actions",
     "@storybook/addon-links",
+    "storybook-css-modules-preset",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   typescript: {
     check: false,
