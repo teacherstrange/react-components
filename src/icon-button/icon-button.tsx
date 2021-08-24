d189ba79701a1b7d4d8147a9f7ffe89d512d8afb
Mattia <astorino.mattia@gmail.com>
2021-08-24 16:02:48 +0200

    fix: test polymorphism on Button and IconButton

--- src/icon-button/icon-button.tsx
+++ src/icon-button/icon-button.tsx
@@ -1,17 +1,20 @@
 import React, { forwardRef } from 'react'
 import { IconButton as IconButtonClass } from './icon-button.module.css'
 import { Button, ButtonProps } from '../button'
+import type * as Polymorphic from '@radix-ui/react-polymorphic'
 import clsx from 'clsx'
-import { IconNames } from 'src/icons/types'
 
-export type IconButtonProps = PropsWithClass & {
-  kind?: ButtonProps['kind'];
-  dimension?: ButtonProps['dimension'];
-  icon: IconNames,
-  disabled?: ButtonProps['disabled'];
-  onClick?: ButtonProps['onClick'];
-}
+export type IconButtonOwnProps = PropsWithClass & Pick<
+  ButtonProps,
+  'kind' | 'dimension' | 'icon' | 'disabled' | 'onClick'
+>
 
+type PolymorphicIconButton = Polymorphic.ForwardRefComponent<
+  Polymorphic.IntrinsicElement<typeof Button>,
+  Polymorphic.OwnProps<typeof Button> & IconButtonOwnProps
+>;
+
+// eslint-disable-next-line react/display-name
 export const IconButton = forwardRef(({
   className,
   icon,
@@ -19,9 +22,9 @@ export const IconButton = forwardRef(({
   kind,
   disabled,
   ...props
-}: IconButtonProps, ref: any) => (
+}, forwardedRef) => (
   <Button
-    ref={ref}
+    ref={forwardedRef}
     icon={icon}
     dimension={dimension}
     kind={kind}
@@ -29,6 +32,6 @@ export const IconButton = forwardRef(({
     className={clsx(IconButtonClass, className)}
     {...props}
   />
-))
+)) as PolymorphicIconButton
 
 IconButton.displayName = 'IconButton'
