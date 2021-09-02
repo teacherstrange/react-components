3ed6a4c6331df48f491222dff8b056ce58733ce3
Mattia <astorino.mattia@gmail.com>
2021-09-02 11:29:51 +0200

    feat: add Elevator component

---
+++ src/elevator/elevator.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Elevator } from './elevator'
+
+export default {
+  title: 'Components/Elevator',
+  component: Elevator
+}
+
+const Template = (args) => (
+  <Elevator {...args}>
+    <div style={{ padding: 32, background: 'var(--dimmed-2)' }}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, unde.</div>
+  </Elevator>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  resting: 1,
+  hover: 3
+}
---
+++ src/elevator/elevator.tsx
@@ -0,0 +1,24 @@
+import React, { Fragment, Children, cloneElement, ReactElement } from 'react'
+
+export type ElevatorProps = {
+  resting: 1 | 2 | 3 | 4;
+  hover?: 1 | 2 | 3 | 4;
+}
+
+export const Elevator: React.FC<ElevatorProps> = ({
+  children,
+  resting,
+  hover
+}) => {
+  return (
+    <Fragment>
+      {Children.map(children, (child: ReactElement) => cloneElement(
+        child,
+        {
+          'data-elevation': resting,
+          'data-elevation-hover': hover
+        }
+      ))}
+    </Fragment>
+  )
+}
