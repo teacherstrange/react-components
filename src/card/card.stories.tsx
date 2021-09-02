95897338aca425c6a3c7d6f29a740ba19db60d6d
Mattia <astorino.mattia@gmail.com>
2021-09-02 14:28:27 +0200

    chore: clean Card component

--- src/card/card.stories.tsx
+++ src/card/card.stories.tsx
@@ -3,6 +3,7 @@ import { Card } from './card'
 import { Text } from '../text'
 import { Title } from '../title'
 import { Icon } from '../icon'
+import { Stack } from '../stack'
 
 export default {
   title: 'Components/Card',
@@ -18,33 +19,39 @@ export default {
 
 const Template = (args) => <Card dimmed={1} {...args} />
 
-export const WithChildren = Template.bind({})
+export const Default = Template.bind({})
 /* eslint-disable */
-WithChildren.args = {
+Default.args = {
   children: <>
     <Title level="3">Card title</Title>
     <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
   </>
 }
 
-export const WithLeftSlot = Template.bind({})
-WithLeftSlot.args = {
+export const WithLeft = Template.bind({})
+WithLeft.args = {
   left: <Icon name="bell" dimension={32} />,
-  lineOne: <Title level="5">Title</Title>,
-  lineTwo: <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
+  children: <Stack>
+    <Title level="5">Title</Title>
+    <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
+  </Stack>
 }
 
-export const WithRightSlot = Template.bind({})
-WithRightSlot.args = {
+export const WithRight = Template.bind({})
+WithRight.args = {
   right: <Icon name="bell" dimension={32} />,
-  lineOne: <Title level="5">Title</Title>,
-  lineTwo: <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
+  children: <Stack>
+    <Title level="5">Title</Title>
+    <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
+  </Stack>
 }
 
-export const WithSlots = Template.bind({})
-WithSlots.args = {
+export const WithLeftAndRight = Template.bind({})
+WithLeftAndRight.args = {
   left: <Icon name="bell" dimension={32} />,
-  lineOne: <Title level="5">Title</Title>,
-  lineTwo: <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>,
+  children: <Stack>
+    <Title level="5">Title</Title>
+    <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</Text>
+  </Stack>,
   right: <Icon name="calendar" dimension={32} />
 }
--- src/card/card.tsx
+++ src/card/card.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, forwardRef, ReactNode, isValidElement } from 'react'
+import React, { CSSProperties, forwardRef, ReactNode } from 'react'
 import type * as Polymorphic from '@radix-ui/react-polymorphic'
 import tkns from '@wonderflow/tokens/platforms/web/tokens.json'
 import { Stack, StackProps } from '../stack'
@@ -12,8 +12,6 @@ StackProps,
   'wrap' | 'columnGap' | 'verticalAlign' | 'horizontalAlign'> & {
   padding?: false | TokensTypes['space'];
   left?: ReactNode;
-  lineOne?: ReactNode;
-  lineTwo?: ReactNode;
   right?: ReactNode;
   radius?: false | TokensTypes['radius'];
   dimmed?: 1 | 2 | 3;
@@ -29,8 +27,6 @@ export const Card = forwardRef(({
   padding = 24,
   radius = 8,
   left,
-  lineOne,
-  lineTwo,
   right,
   dimmed,
   columnGap = 24,
@@ -53,29 +49,28 @@ export const Card = forwardRef(({
       data-card-dimmed={dimmed}
       {...props}
     >
-      {children || (
-        <Stack
-          direction="row"
-          fill={false}
-          verticalAlign={verticalAlign}
-          horizontalAlign={horizontalAlign}
-          columnGap={columnGap}
-          wrap={wrap}
-        >
-          {left && <div className={Left}>{left}</div>}
+      <Stack
+        direction="row"
+        fill={false}
+        verticalAlign={verticalAlign}
+        horizontalAlign={horizontalAlign}
+        columnGap={columnGap}
+        wrap={wrap}
+      >
+        {left && <div className={Left}>{left}</div>}
 
+        {children && (
           <Stack className={Content}>
-            {lineOne && isValidElement(lineOne) ? lineOne : <span>{lineOne}</span>}
-            {lineTwo && isValidElement(lineTwo) ? lineTwo : <span>{lineTwo}</span>}
+            {children}
           </Stack>
+        )}
 
-          {right && (
+        {right && (
           <Stack className={Right}>
             {right}
           </Stack>
-          )}
-        </Stack>
-      )}
+        )}
+      </Stack>
     </Wrapper>
   )
 }) as PolymorphicCard
