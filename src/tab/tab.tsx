9e55653ad8a4f69ff19295e7731a93d99f3c134d
Mattia <astorino.mattia@gmail.com>
2021-09-08 12:01:41 +0200

    chore: refactor tab code style

--- src/tab/tab.tsx
+++ src/tab/tab.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import React, {
   useCallback,
   useEffect,
@@ -18,116 +19,174 @@ import type * as Polymorphic from '@radix-ui/react-polymorphic'
 
 import {
   Tab as TabClass,
-  TabList,
-  TabItem,
-  TabPanel
+  TabList as TabListClass,
+  TabItem as TabItemClass,
+  TabPanel as TabPanelClass
 } from './tab.module.css'
 
+/* -------------------------------------------------------------------------- */
+/*                                  Tab.Root                                  */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Tab.Root
+ * Public api
+ */
 type TabProps = PropsWithClass & {
   children: ReactNode;
   state?: [number, React.Dispatch<React.SetStateAction<number>>];
   onChange?: (index: number) => void;
 }
 
-type PolymorphicTabItem = Polymorphic.ForwardRefComponent<'button', {}>;
+/**
+ * Tab.Root
+ * Component
+ */
+const TabRoot: React.FC<TabProps> = forwardRef(({
+  children,
+  className,
+  state,
+  onChange,
+  ...props
+}, forwardedRef: Ref<HTMLDivElement>) => {
+  const innerState = useState(0)
+  const tabState = state || innerState
+  const [currentTab] = tabState
 
-export const Tab: {
-  Root: React.FC<TabProps>;
-  List: React.FC<PropsWithClass>
-  Panel: React.FC<PropsWithClass>
-  Item: PolymorphicTabItem;
-} = {
-  Root: forwardRef(({
-    children,
-    className,
-    state,
-    onChange,
-    ...props
-  }, forwardedRef: Ref<HTMLDivElement>) => {
-    const innerState = useState(0)
-    const tabState = state || innerState
-    const [currentTab] = tabState
+  useEffect(() => {
+    if (typeof onChange === 'function') {
+      onChange(currentTab)
+    }
+  }, [currentTab, onChange])
 
-    useEffect(() => {
-      if (typeof onChange === 'function') {
-        onChange(currentTab)
-      }
-    }, [currentTab, onChange])
+  return (
+    <div
+      ref={forwardedRef}
+      className={clsx(TabClass, className)}
+      {...props}
+    >
+      <TabsWrapper state={tabState}>
+        {children}
+      </TabsWrapper>
+    </div>
+  )
+})
 
-    return (
+/* -------------------------------------------------------------------------- */
+/*                                  Tab.List                                  */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Tab.List
+ * Component
+ */
+const TabList: React.FC<PropsWithClass> = forwardRef(({
+  children,
+  className,
+  ...props
+}, forwardedRef: Ref<HTMLDivElement>) => (
+  <div
+    ref={forwardedRef}
+    role="tablist"
+    tabIndex={-1}
+    className={clsx(TabListClass, className)}
+    {...props}
+  >
+    <RovingTabIndexProvider>
+      {children}
+    </RovingTabIndexProvider>
+  </div>
+))
+
+/* -------------------------------------------------------------------------- */
+/*                                  Tab.Panel                                 */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Tab.Panel
+ * Component
+ */
+const TabPanel: React.FC<PropsWithClass> = forwardRef(({
+  children,
+  className,
+  ...props
+}, forwardedRef: Ref<HTMLDivElement>) => {
+  const isActive = usePanelState()
+  return isActive
+    ? (
       <div
         ref={forwardedRef}
-        className={clsx(TabClass, className)}
+        tabIndex={0}
+        className={clsx(TabPanelClass, className)}
         {...props}
       >
-        <TabsWrapper state={tabState}>
-          {children}
-        </TabsWrapper>
+        {children}
       </div>
-    )
-  }),
+      )
+    : null
+})
 
-  List: forwardRef(({ children, className, ...props }, forwardedRef: Ref<HTMLDivElement>) => (
-    <div ref={forwardedRef} role="tablist" tabIndex={-1} className={clsx(TabList, className)} {...props}>
-      <RovingTabIndexProvider>
-        {children}
-      </RovingTabIndexProvider>
-    </div>
-  )),
+/* -------------------------------------------------------------------------- */
+/*                                  Tab.Item                                  */
+/* -------------------------------------------------------------------------- */
 
-  Panel: forwardRef(({ children, className, ...props }, forwardedRef: Ref<HTMLDivElement>) => {
-    const isActive = usePanelState()
-    return isActive
-      ? (
-        <div
-          ref={forwardedRef}
-          tabIndex={0}
-          className={clsx(TabPanel, className)}
-          {...props}
-        >
-          {children}
-        </div>
-        )
-      : null
-  }),
+/**
+ * Tab.Item
+ * Public api
+ */
+type PolymorphicTabItem = Polymorphic.ForwardRefComponent<'button', {}>;
 
-  Item: forwardRef(({
-    children,
-    className,
-    as: Wrapper = 'button',
-    ...props
-  }, forwardedRef) => {
-    const { onClick, isActive } = useTabState()
-    const internalRef = useRef<Polymorphic.IntrinsicElement<typeof Wrapper>>(null)
-    const [, focused, handleKeyDown, handleClick] = useRovingTabIndex(internalRef, false)
+/**
+ * Tab.Item
+ * Component
+ */
+const TabItem = forwardRef(({
+  children,
+  className,
+  as: Wrapper = 'button',
+  ...props
+}, forwardedRef) => {
+  const { onClick, isActive } = useTabState()
+  const internalRef = useRef<Polymorphic.IntrinsicElement<typeof Wrapper>>(null)
+  const [, focused, handleKeyDown, handleClick] = useRovingTabIndex(internalRef, false)
 
-    useFocusEffect(focused, internalRef)
+  useFocusEffect(focused, internalRef)
 
-    const fireClick = useCallback(
-      () => {
-        onClick()
-        handleClick()
-      },
-      [handleClick, onClick]
-    )
+  const fireClick = useCallback(
+    () => {
+      onClick()
+      handleClick()
+    },
+    [handleClick, onClick]
+  )
 
-    return (
-      <Wrapper
-        role="tab"
-        ref={forwardedRef || internalRef}
-        className={clsx(TabItem, className)}
-        aria-selected={isActive}
-        onClick={fireClick}
-        onKeyDown={handleKeyDown}
-        onFocus={fireClick}
-        type={Wrapper === 'button' ? 'button' : undefined}
-        tabIndex={isActive ? 0 : -1}
-        {...props}
-      >
-        {children}
-      </Wrapper>
-    )
-  })
+  return (
+    <Wrapper
+      role="tab"
+      ref={forwardedRef || internalRef}
+      className={clsx(TabItemClass, className)}
+      aria-selected={isActive}
+      onClick={fireClick}
+      onKeyDown={handleKeyDown}
+      onFocus={fireClick}
+      type={Wrapper === 'button' ? 'button' : undefined}
+      tabIndex={isActive ? 0 : -1}
+      {...props}
+    >
+      {children}
+    </Wrapper>
+  )
+}) as PolymorphicTabItem
+
+/* -------------------------------------------------------------------------- */
+/*                                   Export                                   */
+/* -------------------------------------------------------------------------- */
+
+export const Tab = {
+  Root: TabRoot,
+  List: TabList,
+  Item: TabItem,
+  Panel: TabPanel
 }
 
 Tab.Root.displayName = 'Tab.Root'
