0fa10aad58d706c50f05438c4b42a3e11a06ef02
Mattia <astorino.mattia@gmail.com>
2021-09-10 12:59:44 +0200

    feat: add Disclosure component

---
+++ src/disclosure/disclosure.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Disclosure } from './disclosure'
+
+export default {
+  title: 'Components/Disclosure',
+  component: Disclosure,
+  args: {
+    padding: true,
+    expandable: true
+  },
+  argTypes: {
+    expandable: {
+      options: [true, false],
+      control: { type: 'inline-radio' }
+    },
+    padding: {
+      options: [true, false],
+      control: { type: 'inline-radio' }
+    }
+  }
+}
+
+const Template = (args) => (
+  <Disclosure summary="Lorem ipsum dolor sit amet consectetur adipisicing elit." {...args}>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Doloribus corporis nobis ipsum natus laudantium dolorem sapiente,
+    sint adipisci at asperiores quia ex voluptatum veniam! Quos rerum natus eos excepturi atque!
+  </Disclosure>
+)
+
+export const Default = Template.bind({})
---
+++ src/disclosure/disclosure.tsx
@@ -0,0 +1,94 @@
+import React, { CSSProperties, DetailsHTMLAttributes, ReactNode, useCallback, useState } from 'react'
+import clsx from 'clsx'
+import { Disclosure as DisclosureClass, Summary, ExpandIcon, Content } from './disclosure.module.css'
+import { Icon } from '../icon'
+import { Text, TextProps } from '../text'
+
+export type DisclosureProps = DetailsHTMLAttributes<HTMLDetailsElement> & {
+  open?: boolean;
+  summary: ReactNode;
+  padding?: boolean;
+  contentMaxHeight?: string;
+  dimension?: 'small' | 'regular' | 'big';
+  iconPosition?: 'left' | 'right';
+  expandable?: boolean;
+}
+
+export const Disclosure: React.FC<DisclosureProps> = ({
+  children,
+  open = false,
+  padding = true,
+  className,
+  summary,
+  contentMaxHeight,
+  dimension = 'regular',
+  iconPosition = 'left',
+  expandable = true,
+  style,
+  ...props
+}) => {
+  const [isOpen, setIsOpen] = useState(open)
+
+  const handleOpen = useCallback(
+    () => (event: any) => {
+      event.preventDefault()
+      expandable && setIsOpen(!isOpen)
+    },
+    [isOpen, expandable]
+  )
+
+  const dynamicStyle: CSSProperties = {
+    '--maxHeight': contentMaxHeight
+  }
+
+  const sizes = {
+    small: {
+      summary: 16
+    },
+    regular: {
+      summary: 18
+    },
+    big: {
+      summary: 22
+    }
+  }
+
+  return (
+    <details
+      style={{ ...dynamicStyle, ...style }}
+      className={clsx(DisclosureClass, className)}
+      data-disclosure-icon-position={iconPosition}
+      data-disclosure-dimension={dimension}
+      data-disclosure-expandable={expandable}
+      aria-expanded={isOpen ? 'true' : 'false'}
+      open={isOpen}
+      {...props}
+    >
+      <Text
+        as="summary"
+        onClick={handleOpen()}
+        className={Summary}
+        fluid={false}
+        tabIndex={!expandable ? -1 : undefined}
+        size={dimension ? sizes[dimension].summary as TextProps['size'] : undefined}
+        weight="bold"
+      >
+        {summary}
+        {expandable && (
+        <Icon
+          className={ExpandIcon}
+          name="chevron-up"
+          dimension={dimension !== 'big' ? 16 : 24}
+        />
+        )}
+      </Text>
+      <div
+        className={Content}
+        data-disclosure-padding={padding}
+        data-disclosure-height={Boolean(contentMaxHeight)}
+      >
+        {children}
+      </div>
+    </details>
+  )
+}
