a1f069823ee2068fc7e907d42891362d28b4e245
Mattia <astorino.mattia@gmail.com>
2021-10-13 15:13:20 +0200

    feat: add toggle-button component

---
+++ src/toggle-button/toggle-button.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { ToggleButton } from './toggle-button'
+
+export default {
+  title: 'Components/Toggle button',
+  component: ToggleButton,
+  args: {
+    dimension: 'regular',
+    kind: 'primary',
+    restingIcon: 'play'
+  },
+  argTypes: {
+    onClick: { action: 'clicked' },
+    dimension: {
+      options: ['small', 'regular', 'big'],
+      control: { type: 'radio' }
+    },
+    kind: {
+      options: ['primary', 'secondary', 'flat'],
+      control: { type: 'radio' }
+    }
+  }
+}
+
+const Template = (args) => <ToggleButton {...args} />
+
+export const Default = Template.bind({})
+
+export const MultipleIcons = Template.bind({})
+MultipleIcons.args = {
+  pressedIcon: 'pause'
+}
