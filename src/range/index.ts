7459c9f15ab837200e91240b9f55458f26125d9b
Mattia <astorino.mattia@gmail.com>
2021-11-03 08:27:59 +0100

    feat: add range component

---
+++ src/range/index.ts
@@ -0,0 +1,2 @@
+export { Range } from './range'
+export type { RangeProps } from './range'
---
+++ src/range/range.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Range } from './range'
+
+export default {
+  title: 'Components/Inputs/Range',
+  component: Range,
+  args: {
+    min: 0,
+    max: 100,
+    step: 1,
+    defaultValue: 50,
+    showValues: false,
+    dimension: 'regular',
+    disabled: false
+  },
+  argTypes: {
+    onInput: {
+      action: 'changed',
+      table: {
+        disable: true
+      }
+    },
+    showValues: {
+      options: [true, false],
+      control: { type: 'inline-radio' }
+    }
+  }
+}
+
+const Template = (args) => <Range {...args} />
+
+export const Default = Template.bind({})
+export const WithValues = Template.bind({})
+WithValues.args = {
+  showValues: true
+}
+export const WithIcons = Template.bind({})
+WithIcons.args = {
+  iconLeft: 'moon',
+  iconRight: 'sun-bright'
+}
