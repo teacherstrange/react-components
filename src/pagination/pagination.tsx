efd88ddf4be0e5d25be03b30edf5c054569893b5
Mattia <astorino.mattia@gmail.com>
2021-12-20 15:51:15 +0100

    feat(pagination): add prop descriptions

--- src/pagination/pagination.tsx
+++ src/pagination/pagination.tsx
@@ -7,11 +7,33 @@ import { Icon } from '../'
 import { Pagination as PaginationClass } from './pagination.module.css'
 
 export type PaginationProps = Except<ReactPaginateProps, 'pageCount'> & {
+  /**
+   * Set the total number of items to paginate through.
+   */
   itemsCount: number;
+  /**
+   * Set the number of items to display per page.
+   */
   itemsPerPage?: number;
+  /**
+   * Set the number of pages to display. If missing this is computed by
+   * the `itemsCount` divided by `itemsPerPage`.
+   */
   pageCount?: number;
+  /**
+   * Callback function to be called when the page is changed. A an `object`
+   * is passed with the following properties:
+   * - `selected`: The index of the selected page.
+   * - `offset`: The offset of the selected page.
+   */
   onPageClick?(data: Record<string, number>): void;
+  /**
+   * Set how many pages to show in the visible page range (between the "..." break)
+   */
   pageRangeDisplayed?: ReactPaginateProps['pageRangeDisplayed'];
+  /**
+   * The number of visible pages to display on the sides.
+   */
   marginPagesDisplayed?: ReactPaginateProps['marginPagesDisplayed'];
 }
 
