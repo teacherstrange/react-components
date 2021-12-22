5d1e1a936e77f05fcfdbbbc96fdb0efa3d11e0af
Mattia <astorino.mattia@gmail.com>
2021-12-22 10:59:52 +0100

    chore: update storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,36 +1,32 @@
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
-  addons: [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "storybook-css-modules-preset",
-    'storybook-addon-themes',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        backgrounds: false,
+  core: {
+    builder: "webpack5",
+  },
+  addons: ["@storybook/addon-actions", "@storybook/addon-links", "storybook-css-modules-preset", 'storybook-addon-themes', {
+    name: '@storybook/addon-essentials',
+    options: {
+      backgrounds: false
+    }
+  }, {
+    name: '@storybook/addon-postcss',
+    options: {
+      postcssLoaderOptions: {
+        implementation: require('postcss')
       }
-    },
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
-    "storybook-addon-turbo-build"
-  ],
+    }
+  }, "storybook-addon-turbo-build"],
   typescript: {
     check: false,
     checkOptions: {},
     reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
+      propFilter: prop => prop.parent ? !/node_modules/.test(prop.parent.fileName) : true
+    }
   },
+  framework: "@storybook/react"
 };
